5d29fdc2991eceb42d12fbe1650ddc0e6c1dc55d
nuilewis <lewisnuikweh@gmail.com>
2022-03-15 14:19:54 +0100

    Initialized Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cleancity/screens/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
+  Firebase.initializeApp();
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
