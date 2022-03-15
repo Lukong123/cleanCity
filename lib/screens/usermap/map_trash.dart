70a3903d4ece639860241e26ed213f6ae257ef12
godsakani <nyonggodwill11@gmail.com>
2022-03-15 16:20:28 +0100

    Added more locations and modify title on marker

--- lib/screens/usermap/map_trash.dart
+++ lib/screens/usermap/map_trash.dart
@@ -9,14 +9,12 @@ class MapTrash extends StatefulWidget {
 
 class _MapTrashState extends State<MapTrash> {
   Set<Marker> yourMarkers = {};
-
-  //defining the map Controller
+  ///defining the map Controller
   late GoogleMapController myController;
 
-  //setting the marker
+  ///setting the marker
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
-  //get firebaseDocument => ["location"];
   //Creating a method for the marker
   void initMarker(specify, specifyId) async {
     var markerIdVal = specifyId;
@@ -26,7 +24,8 @@ class _MapTrashState extends State<MapTrash> {
         position:
             LatLng(specify['location'].latitude, specify['location'].longitude),
         infoWindow: InfoWindow(title: 'Trash', snippet: specify['address']));
-    //setting the state of the method
+
+    ///setting the state of the method
     setState(() {
       markers[markerId] = marker;
     });
@@ -43,22 +42,27 @@ class _MapTrashState extends State<MapTrash> {
 
     ///looping through the list
     for (int i = 0; i < firebaseDocument.length; i++) {
-//print("data from firesote is ${firebaseDocument[i]["location"].latitude}");
+
+      ///print("data from firesote is ${firebaseDocument[i]["location"].latitude}");
 
       ///Getting the markers
       MarkerId markerId = MarkerId(firebaseDocument[i].id);
       yourMarkers.add(
         Marker(
-            markerId: markerId,
-            position: LatLng(firebaseDocument[i]["location"].latitude,
-                firebaseDocument[i]["location"].longitude)),
+          markerId: markerId,
+          position: LatLng(firebaseDocument[i]["location"].latitude,
+              firebaseDocument[i]["location"].longitude),
+          icon: BitmapDescriptor.defaultMarker,
+          infoWindow: InfoWindow(title: firebaseDocument[i]["addresses"]),
+        ),
       );
-      // print("your markers set is : $yourMarkers");
+
+      /// print("your markers set is : $yourMarkers");
       setState(() {});
     }
   }
 
-  //Getting the main branch of the children widget
+  ///Getting the main branch of the children widget
   void initState() {
     getMarkerData();
     super.initState();
@@ -70,7 +74,7 @@ class _MapTrashState extends State<MapTrash> {
     Set<Marker> setMarkers() {
       return yourMarkers;
     }
-
+/*
     Set<Marker> getMarker() {
       return <Marker>[
         Marker(
@@ -81,21 +85,22 @@ class _MapTrashState extends State<MapTrash> {
       ].toSet();
     }
 
+ */
+
     return Scaffold(
         body: GoogleMap(
-      markers: setMarkers(),
-      myLocationEnabled: true,
-      mapType: MapType.normal,
-      initialCameraPosition: CameraPosition(
-        target: LatLng(4.161385512444317, 9.287080154746542),
-        zoom: 14.0,
-      ),
-      //calling the controller
-      onMapCreated: (GoogleMapController controller) {
-        myController = controller;
-      },
-    ));
+          markers: setMarkers(),
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(4.161385512444317, 9.287080154746542),
+            zoom: 14.0,
+          ),
+          //calling the controller
+          onMapCreated: (GoogleMapController controller) {
+            myController = controller;
+          },
+        )
+    );
   }
 }
-
-//Set<Marker>.of(markers.values)
